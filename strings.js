082ef4358de6216f070536d81047e7d1c639a2f7
TylerAycock <tyler.joseph.aycock@gmail.com>
2024-10-08 11:51:04 -0700

    finished string problems

--- strings.js
+++ strings.js
@@ -5,55 +5,44 @@
 // *** Problem 1 ***
 //  Students should be familiar with and
 //  know how to use a variety of string methods.
-//  They should also be able to find and use resources
-//  that teach them how to use new string methods.
 
 // 1.1a
 //  Given a string, str1, find the third character
 //  using a string method and save it to a new variable.
 
-const str1 = "Howdy, there!"
+const str1 = "Howdy, there!";
 
+let findChar = (str) => {
+  return str.charAt(2);
+};
 
-// 1.1b
-//  Find a URL that has documentation or usage examples for
-//  the string method used to solve 1.1a and put it
-//  here in a JS comment (an MDN link is usually a good choice).
+console.log(findChar(str1));
 
-
-
-// 1.2a
+// 1.2
 //  Given a string, str2, make a new string that is an
 //  all-caps version of it using a string method.
 
-const str2 = "I forgot to turn off the oven!"
+const str2 = "I forgot to turn off the oven!";
 
+let allCap = (str) => {
+  return str.toUpperCase();
+};
 
-// 1.2b
-//  Find a URL that has documentation or usage examples for
-//  the string method used to solve 1.2a and put it
-//  here in a JS comment (an MDN link is usually a good choice).
-
-
+console.log(allCap(str2));
 
 // 1.3
-//  Using your Google sleuthing skills, find out what the .trim()
-//  method does, and explain it in comments below, in your own words.
-
-
-
-// 1.4
-//  Using your Google sleuthing skills, find a JS string method
-//  that will find a specified substring in a string, and replace
+//  Given a string, str3, make a new string that replaces
 //  all of the occurences of that substring with another substring.
-//  For example, given the string
-//    "I love to eat apples and bananas",
+
+let str3 = "I love to eat apples and bananas";
+
 //  changing all of the "a" substrings to "u" substrings
-//  will result in the string
-//    "I love to eut upples and bununus".
-//  Put the name of the method you find in a comment below.
 
+let changeLetters = (str, oldLetter, newLetter) => {
+  return str.replace(oldLetter, newLetter);
+};
 
+console.log(changeLetters(str3, "a", "u"));
 
 // *** Problem 2 ***
 //  Students should know how to concatenate strings.
@@ -64,15 +53,11 @@ const str2 = "I forgot to turn off the oven!"
 //  You must use the variables provided to form the output string.
 //  To accomplish this, use string concatenation.
 
-const first = 'Johnny'
-const last = 'Paboni'
-const num = 2
-const phrase = 'want to mess'
+const first = "Johnny";
+const last = "Paboni";
+const num = 2;
+const phrase = "want to mess";
 
-
-// 2.2
-//  Do problem 2.1 by doing another way of string concatenation.
-//  For example, if the '+' operator was used, perhaps template
-//  literals should be used this time.
-
-
+console.log(
+  `My number ${num}'s named "${first} ${last}" and you don't ${phrase}`
+);
